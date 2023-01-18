ad3581dd1462dfc911a12ff8dd40ad4932d48e5f
tiznadoc <tiznadoc@gmail.com>
2023-01-17 21:45:09 -0300

    parte-2 Desafio-2 added

--- DL-JS-para-la-web-Desafio-2/assets/JS/main.js
+++ DL-JS-para-la-web-Desafio-2/assets/JS/main.js
@@ -9,3 +9,17 @@ document.getElementById("tere").addEventListener("click", function(){
         border = true
     }
 })
+
+function sumar(){
+    let suma = parseInt(document.getElementById("wave").value) + parseInt(document.getElementById("hand").value) + parseInt(document.getElementById("shop").value)
+    return suma
+}
+
+document.getElementById("submit").addEventListener("click", ()=>{
+    let suma = sumar()
+    if (suma > 10){
+        alert("Demasiados Stickers")
+        document.getElementById("submit").className = "btn btn-secondary text disabled"
+    }
+    document.getElementById("total").innerText = suma
+})
